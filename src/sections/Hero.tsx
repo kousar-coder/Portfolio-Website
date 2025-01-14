98d82f57607aebef76fbab5147c18370694630a1
kousar saeed <kousarsaeed.narejo@gmail.com>
2025-01-14 17:09:05 +0500

    commit

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,10 +4,13 @@ import ArrowDown from '@/assets/icons/arrow-down.svg';
 import grainImage from '@/assets/images/grain.jpg';
 import StarIcon from '@/assets/icons/star.svg';
 import { HeroOrbit } from '@/components/HeroOrbit';
+import SparkleIcon from '@/assets/icons/sparkle.svg';
+
 
 export const HeroSection = () => {
   return (
     <div className="py-32 md:py-48 lg:py-60 realtive z-0 overflow-x-clip"> 
+    <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
     <div className="absolute inset-0 -z-30 opacity-5" style={{
       backgroundImage:`url(${grainImage.src})`
     }}>
@@ -26,11 +29,33 @@ export const HeroSection = () => {
       <HeroOrbit size={590} rotation={98}>
         <StarIcon className="size-8 text-emerald-300" />
       </HeroOrbit>
+      <HeroOrbit size={430} rotation={-14}>
+        <SparkleIcon className="size-8 text-emerald-300/20" />
+      </HeroOrbit>
+      <HeroOrbit size={440} rotation={79}>
+        <SparkleIcon className="size-5 text-emerald-300/20" />
+      </HeroOrbit>
+      <HeroOrbit size={530} rotation={178}>
+        <SparkleIcon className="size-10 text-emerald-300/20" />
+      </HeroOrbit>
+      <HeroOrbit size={710} rotation={144}>
+        <SparkleIcon className="size-14 text-emerald-300/20" />
+      </HeroOrbit>
+      <HeroOrbit size={720} rotation={85}>
+        <div className="size-3 rounded-full bg-emerald-300/20" />
+      </HeroOrbit>
+      <HeroOrbit size={520} rotation={-14}>
+        <div className="size-2 rounded-full bg-emerald-300/20" />
+      </HeroOrbit>
+      <HeroOrbit size={650} rotation={-5}>
+        <div className="size-2 rounded-full bg-emerald-300/20" />
+      </HeroOrbit>
+      </div>
       <div className="container ">
         <div className="flex flex-col items-center">
           <Image
             src={memojiImage}
-            className="w-[100px] h-[100px] rounded-full"
+            className="w-[170px] h-[170px] rounded-full"
             alt="Person peeking from behind laptop"
           />
           <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-2 rounded-lg mt-4">
